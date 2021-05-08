12b40d5d71fbf358fc8ce0a81f0099b8aa90efe6
Jhoselynne <jhoselynne@hotmail.com>
2021-05-09 00:20:35 +0200

    Added class Person

--- Program.cs
+++ Program.cs
@@ -2,6 +2,17 @@
 
 namespace myApp
 {
+    class Person
+    {
+        public string namn;
+        public int year = 0;
+
+        public Person(string namn)
+        {
+            this.namn = namn;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
@@ -10,16 +21,20 @@ namespace myApp
             string message = "Bananer";
             string pappa = "Äpplen";
 
+            Person person1 = new Person("Christian");
+
             if (areEqual)
             {
                 Console.WriteLine("areEqual innehåller " + areEqual);
                 Console.WriteLine("message innehåller " + message);
                 Console.WriteLine("10 = 9 är " + (10 == 9));
+                Console.WriteLine("person1.namn: " + person1.namn);
+                Console.WriteLine("person1.year: " + person1.year);
                 // Console.WriteLine("The current time is " + DateTime.Now);
             }
             else
             {
-                Console.WriteLine(pappa);                
+                Console.WriteLine(pappa);
             }
 
         }
